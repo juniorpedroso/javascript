3b8e4d1de1663723b65bcd80e8e54873a57f090c
juniorpedroso <sintasebemagora@gmail.com>
2022-04-15 10:36:24 -0300

    Update

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 70
+if (idade < 18) {
+    console.log('adolescente')
+} else {
+    if (idade < 65) {
+        console.log('adulto')
+    } else {
+        console.log('idoso')
+    }
+}
---
+++ aula12/ex011b.js
@@ -0,0 +1,9 @@
+var idade = 60
+if (idade < 18) {
+    console.log('adolescente')
+} else if (idade < 65) {
+        console.log('adulto')
+} else {
+        console.log('idoso')
+}
+
