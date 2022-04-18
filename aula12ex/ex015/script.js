0429630e727b0a52d514883de026fbcd3d7385b9
Junior Pedroso <sintasebemagora@gmail.com>
2022-04-18 15:37:16 -0300

    Update

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,9 +3,51 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value) >= ano) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('Verifique os dados e tente novamente!')
     } else {
-        window.alert('Tudo ok!')
+        // window.alert(`Tudo OK`)
+        // window.alert(idade)
+        var fsex = document.getElementsByName('radsex')
+        var idade = Number(ano) - Number(fano.value)
+        var genero = ''
+        // Criando um elemento imagem para a foto
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'image/foto-bebe-m.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'image/foto-jovem-m.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'image/foto-adulto-m.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'image/foto-idoso-m.png')
+            }
+        } else {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'image/foto-bebe-f.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'image/foto-jovem-f.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'image/foto-adulto-f.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'image/foto-idoso-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        // Exibe a imagem
+        res.appendChild(img)
     }
 }
