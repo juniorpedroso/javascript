530504727c97e5da0bd7f859dfeb7f63e1cea36b
juniorpedroso <sintasebemagora@gmail.com>
2022-05-05 08:42:25 -0300

    Create teste.js

---
+++ java_O_Guia_Definitivo/teste_json001/teste.js
@@ -0,0 +1,19 @@
+let arr = ["Carlos", "Pedro", "Alfredo"];
+console.log('Array')
+console.log(arr)
+
+let myJSON = JSON.stringify(arr)
+console.log('JSON')
+console.log(myJSON)
+
+let obj = {
+    name: 'Carlos',
+    age: 29,
+    profession: 'Engineer'
+}
+console.log('objeto')
+console.log(obj)
+let myJSON2 = JSON.stringify(obj)
+console.log('JSON')
+console.log(myJSON2)
+
